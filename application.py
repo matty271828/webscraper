6fe194ff4525b21140301c660d95e6e8ea9cfaa0
Matthew Maclean <macleanmatty@gmail.com>
2021-05-09 15:57:45 +0100

    Fixed popup close

--- application.py
+++ application.py
@@ -30,10 +30,12 @@ def start_kayak(city_from, city_to, date_start, date_end):
 
     # sometimes a popup shows up, so we can use a try statement to check it and close
     try:
-        xp_popup_close = '//button[contains(@id,"dialog-close") and contains(@class,"Button-No-Standard-Style close ")]'
+        xp_popup_close = '//button[contains(@id,"dialog-close") and contains(@class,"Button-No-Standard-Style")]'
         driver.find_elements_by_xpath(xp_popup_close)[5].click()
+        print("popup closed")
     except Exception as e:
         pass
+
     sleep(randint(1,5))
 
     print('starting first scrape.....')
